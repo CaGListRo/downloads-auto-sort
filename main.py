4ea03e5dfa7938fa9a13f0b24ccaa3d05ab0876e
CaGListRo <paco.nyns@gmail.com>
2025-01-07 11:36:54 +0100

    Added type hints, comments and maked the constants Final.

--- main.py
+++ main.py
@@ -1,27 +1,30 @@
 import os, shutil
+from typing import Final
 
-download_dir = 'C:/Users/admin/Downloads/'
-folder_names = ['PDFs', 'Programme', 'Bilder', 'Videos', 'Zips', 'blender', 'Audio', 'eBooks']
+DOWNLOADS_DIRECTORY: Final[str] = 'C:/Users/admin/Downloads/'
+FOLDER_NAMES: Final[list[str]] = ['PDFs', 'Programme', 'Bilder', 'Videos', 'Zips', 'blender', 'Audio', 'eBooks']
 
-for idx, folder in enumerate(folder_names):
-    if not os.path.exists(download_dir + folder_names[idx]):
-        os.mkdir(download_dir + folder)
+# check if all folders of the FOLDER_NAMES exist, and creates them if not
+for idx, folder in enumerate(FOLDER_NAMES):
+    if not os.path.exists(DOWNLOADS_DIRECTORY + FOLDER_NAMES[idx]):
+        os.mkdir(DOWNLOADS_DIRECTORY + folder)
 
-with os.scandir(download_dir) as entries:
+# sort the files in the downloadfolder according to ending of the file
+with os.scandir(DOWNLOADS_DIRECTORY) as entries:
     for entry in entries:
-        if entry.name.lower().endswith('.pdf') and not os.path.exists(download_dir + 'PDFs/' + entry.name):
-            shutil.move(download_dir + entry.name, download_dir + 'PDFs/' + entry.name)
-        if entry.name.lower().endswith('.exe') and not os.path.exists(download_dir + 'Programme/' + entry.name):
-            shutil.move(download_dir + entry.name, download_dir + 'Programme/' + entry.name)
-        if entry.name.lower().endswith('.jpg') or entry.name.lower().endswith('.png') and not os.path.exists(download_dir + 'Bilder/' + entry.name):
-            shutil.move(download_dir + entry.name, download_dir + 'Bilder/' + entry.name)
-        if entry.name.lower().endswith('.mp4') or entry.name.lower().endswith('.avi') and not os.path.exists(download_dir + 'Videos/' + entry.name):
-            shutil.move(download_dir + entry.name, download_dir + 'Videos/' + entry.name)
-        if entry.name.lower().endswith('.7s') or entry.name.lower().endswith('.zip')or entry.name.lower().endswith('.rar') and not os.path.exists(download_dir + 'Zips/' + entry.name):
-            shutil.move(download_dir + entry.name, download_dir + 'Zips/' + entry.name)
-        if entry.name.lower().endswith('.blend') and not os.path.exists(download_dir + 'blender/' + entry.name):
-            shutil.move(download_dir + entry.name, download_dir + 'blender/' + entry.name)
-        if entry.name.lower().endswith('.mp3') or entry.name.lower().endswith('.wav') or entry.name.lower().endswith('.aac') and not os.path.exists(download_dir + 'Audio/' + entry.name):
-            shutil.move(download_dir + entry.name, download_dir + 'Audio/' + entry.name)
-        if entry.name.lower().endswith('.epub') and not os.path.exists(download_dir + 'eBooks/' + entry.name):
-            shutil.move(download_dir + entry.name, download_dir + 'eBooks/' + entry.name)
+        if entry.name.lower().endswith('.pdf') and not os.path.exists(DOWNLOADS_DIRECTORY + 'PDFs/' + entry.name):
+            shutil.move(DOWNLOADS_DIRECTORY + entry.name, DOWNLOADS_DIRECTORY + 'PDFs/' + entry.name)
+        if entry.name.lower().endswith('.exe') and not os.path.exists(DOWNLOADS_DIRECTORY + 'Programme/' + entry.name):
+            shutil.move(DOWNLOADS_DIRECTORY + entry.name, DOWNLOADS_DIRECTORY + 'Programme/' + entry.name)
+        if entry.name.lower().endswith('.jpg') or entry.name.lower().endswith('.png') and not os.path.exists(DOWNLOADS_DIRECTORY + 'Bilder/' + entry.name):
+            shutil.move(DOWNLOADS_DIRECTORY + entry.name, DOWNLOADS_DIRECTORY + 'Bilder/' + entry.name)
+        if entry.name.lower().endswith('.mp4') or entry.name.lower().endswith('.avi') and not os.path.exists(DOWNLOADS_DIRECTORY + 'Videos/' + entry.name):
+            shutil.move(DOWNLOADS_DIRECTORY + entry.name, DOWNLOADS_DIRECTORY + 'Videos/' + entry.name)
+        if entry.name.lower().endswith('.7s') or entry.name.lower().endswith('.zip') or entry.name.lower().endswith('.rar') and not os.path.exists(DOWNLOADS_DIRECTORY + 'Zips/' + entry.name):
+            shutil.move(DOWNLOADS_DIRECTORY + entry.name, DOWNLOADS_DIRECTORY + 'Zips/' + entry.name)
+        if entry.name.lower().endswith('.blend') and not os.path.exists(DOWNLOADS_DIRECTORY + 'blender/' + entry.name):
+            shutil.move(DOWNLOADS_DIRECTORY + entry.name, DOWNLOADS_DIRECTORY + 'blender/' + entry.name)
+        if entry.name.lower().endswith('.mp3') or entry.name.lower().endswith('.wav') or entry.name.lower().endswith('.aac') and not os.path.exists(DOWNLOADS_DIRECTORY + 'Audio/' + entry.name):
+            shutil.move(DOWNLOADS_DIRECTORY + entry.name, DOWNLOADS_DIRECTORY + 'Audio/' + entry.name)
+        if entry.name.lower().endswith('.epub') and not os.path.exists(DOWNLOADS_DIRECTORY + 'eBooks/' + entry.name):
+            shutil.move(DOWNLOADS_DIRECTORY + entry.name, DOWNLOADS_DIRECTORY + 'eBooks/' + entry.name)
